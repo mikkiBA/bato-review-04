aa3d56f003f346b4b622cb386203a62f57400ffd
mizuki <b.e.p1992bluerose@gmail.com>
2020-03-02 02:57:43 +0900

    コントローラー周り修正

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,25 +1,33 @@
-class ReviewsController < ApplicationController
+class CommentsController < ApplicationController
+    def index
+    end
 
-    
     def show
         @article = Article.find(params[:article_id])
         @starave = Comment.average(:restars)
     end
     
+    def new
+    end
+    
     def create
        @article = Article.find(params[:article_id])
-       @review = @article.comments.create(comment_params)
+       @comment = @article.comments.create(comment_params)
        redirect_to article_path(@article)
-     end
-
+    end
+    
+    def edit
+    end
+    
     def destroy
         @article = Article.find(params[:article_id])
-        @review = @article.comments.find(params[:id])
-        @review.destroy
+        @comment = @article.comments.find(params[:id])
+        @comment.destroy
         redirect_to article_path(@article)
     end
     
      private
+     
      #カラム追加した後ここの許可を忘れるとストロングパラメータに引っかかってDBに保存できない
        def comment_params
          params.require(:comment).permit(:commenter, :body, :restars)
